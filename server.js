7c80f44f9c991f022a5b6f066eec6d71f32fc1f5
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-02-10 07:43:30 -0500

    deploying

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// makes the expresss application USE th route content
+app.use(express.static("public"));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log("am I running");
